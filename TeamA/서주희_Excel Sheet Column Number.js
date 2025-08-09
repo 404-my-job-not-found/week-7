7ad10cb00b374864e833da42a4e791bf41a00976
서주희 <maywngml@naver.com>
2025-08-10 01:51:03 +0900

    refactor: 풀이 방법 수정

--- TeamA/서주희_Excel Sheet Column Number.js
+++ TeamA/서주희_Excel Sheet Column Number.js
@@ -1,14 +1,12 @@
 var titleToNumber = function (columnTitle) {
+  const length = columnTitle.length;
   const aCharCode = 'A'.charCodeAt();
   let result = 0;
-  columnTitle
-    .split('')
-    .reverse()
-    .forEach((char, index) => {
-      const difference = char.charCodeAt() - aCharCode;
 
-      result += (difference + 1) * 26 ** index;
-    });
+  for (let i = length - 1; i >= 0; i--) {
+    const difference = columnTitle[i].charCodeAt() - aCharCode;
+    result += (difference + 1) * 26 ** (length - i - 1);
+  }
 
   return result;
 };
