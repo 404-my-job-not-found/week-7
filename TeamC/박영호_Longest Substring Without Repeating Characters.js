b8491b06813beaca4aa4f8e49d789ea5b82ea7f5
hexdrinker <hexdrinker2020@gmail.com>
2025-08-13 23:25:20 +0900

    solve: Longest Substring Without Repeating Characters

---
+++ TeamC/박영호_Longest Substring Without Repeating Characters.js
@@ -0,0 +1,16 @@
+var lengthOfLongestSubstring = function (s) {
+  let maxLength = 0;
+  let start = 0;
+  const charMap = new Map();
+
+  for (let end = 0; end < s.length; end++) {
+    if (charMap.has(s[end]) && charMap.get(s[end]) >= start) {
+      start = charMap.get(s[end]) + 1;
+    }
+
+    charMap.set(s[end], end);
+    maxLength = Math.max(maxLength, end - start + 1);
+  }
+
+  return maxLength;
+};
